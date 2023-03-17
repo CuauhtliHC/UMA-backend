729680c8e1b94bde6ca8dd84f6eb6ce6c796e43d
Juan Agustin Alejandro <alejandro4juan@gmail.com>
2023-03-17 14:27:38 -0300

    Se borraron comentarios

--- database/conectBD.js
+++ database/conectBD.js
@@ -4,12 +4,6 @@ const dbConection = async () => {
   try {
     await db.sync({ force: false });
     console.log(`Base de datos conectada ${db.config.database}`);
-    // await creationRolesRequired();
-    // await creationStatusOrdersRequired();
-    // await creationUserAdmin();
-    // if (db.config.database === process.env.DATABASE_TEST) {
-    //   await creationUser();
-    // }
   } catch (error) {
     console.log(error);
     throw new Error('error al conectar a la base de datos');
