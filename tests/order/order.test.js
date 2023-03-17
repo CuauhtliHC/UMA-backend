c18cad7994616a7550325c92aa81110c795366cc
Francisco <francisco.leyescampoy@gmail.com>
2023-03-17 17:48:32 -0300

    se creo el test de post y se quedo verificar el delete

--- tests/order/order.test.js
+++ tests/order/order.test.js
@@ -1,11 +1,13 @@
 const supertest = require('supertest');
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
 const Sequelize = require('../../database/database');
 const { server, puertoOpen } = require('../../index');
-const { Order } = require('../../models');
+const { Order, Package } = require('../../models');
 
 const { app, ordersPath, loginPath } = server;
 const api = supertest(app);
-
+dayjs.extend(utc);
 const initOrders = [
   {
     quantity: 2,
@@ -20,14 +22,25 @@ const initOrders = [
     quantity: 5,
   },
 ];
+const newPackage = {
+  address: 'cordoba 2000 1640',
+  addresses: 'Pedro',
+  weight: 12,
+  dateOfDelivery: dayjs().utc().format(),
+  quantityOfPackages: 3,
+};
 let cookieAdmin;
 let cookieUser;
+let newPackageToday;
 
 beforeEach(async () => {
   await Order.sync({ force: true });
+  await Package.sync({ force: true });
   initOrders.forEach(async (initOrder) => {
     await Order.create(initOrder);
   });
+  newPackageToday = await Package.create(newPackage);
+  console.log('ACAAA', newPackageToday);
   const userAdmin = {
     email: 'admin@admin.com',
     password: 'adminUma.',
@@ -66,21 +79,45 @@ describe('orders - GET', () => {
 describe('orders - POST', () => {
   test('Se crea un nuevo Order', async () => {
     const newOrder = {
-      quantity: 456,
+      quantity: 1,
+      packageId: 1,
     };
 
     const prueb = await api
       .post(`${ordersPath}/create/`)
       .send(newOrder)
       .set('Cookie', cookieAdmin.headers['set-cookie']);
-    console.log(prueb);
+    console.log(prueb.body);
     const orders = await Order.findAll();
     const ordersJSON = orders.map((orderData) => orderData.toJSON());
     expect(ordersJSON).toHaveLength(initOrders.length + 1);
   });
 });
 
-afterAll(() => {
-  puertoOpen.close();
-  Sequelize.close();
+describe('Order - Delete', () => {
+  test('Error si el usuario no esta logueado', async () => {
+    const prueb = await api.delete(`${ordersPath}/delete/1`);
+    expect(prueb.status).toBe(401);
+  });
+  test('Error en caso de no enviar un id INTEGER', async () => {
+    const prueb = await api
+      .delete(`${ordersPath}/delete/f`)
+      .set('Cookie', cookieAdmin.headers['set-cookie']);
+    expect(prueb.status).toBe(400);
+  });
+  test('Devuelve Error en caso de no ser Administrador', async () => {
+    await api
+      .delete(`${ordersPath}/delete/1`)
+      .set('Cookie', cookieUser.headers['set-cookie'])
+      .expect(401);
+  });
+  // test('Borra la order', async () => {
+  //   const id = 1;
+  //   const order = await Order.findByPk(id);
+  //   const deleteando = await api
+  //     .delete(`${ordersPath}/delete/${id}`)
+  //     .set('Cookie', cookieAdmin.headers['set-cookie']);
+  //   console.log(order);
+  //   expect(deleteando.body.deleted).toBe(true);
+  // });
 });
