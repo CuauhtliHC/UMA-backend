945dcb0dd4b34c7a793316c30d30ee2e63d2c57f
Cuauhtli Herrera <cuau_daali@hotmail.com>
2023-04-22 20:07:50 +0000

    test dockerfile with env

--- database/conectBD.js
+++ database/conectBD.js
@@ -1,10 +1,16 @@
 const db = require('./database');
+require('dotenv').config();
+
+const {
+  DATABASE, USER, PASSWORD,
+} = process.env;
 
 const dbConection = async () => {
   try {
     await db.sync({ force: false });
     console.log(`Base de datos conectada ${db.config.database}`);
   } catch (error) {
+    console.log(DATABASE, USER, PASSWORD);
     console.log(error);
     throw new Error('error al conectar a la base de datos');
   }
